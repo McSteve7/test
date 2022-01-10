e448c94f98a02945fce4ff4f3c419a72937e00eb
Damilola Ajiboye <34410515+McSteve7@users.noreply.github.com>
2022-01-10 17:42:49 +0100

    Test
    Checking out the build and deploy speed of Gatsby Cloud

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,7 @@ const Footer = () => (
       Built with <a href="https://contentful.com/">Contentful</a> and{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
       <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <a hred="https://damilolaa.xyz">Damilola Ajiboye</a>
     </div>
   </Container>
 )
